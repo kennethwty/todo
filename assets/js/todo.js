1150bc1600f23069fa5b6c609060027998a76048
kennethwty <kennethwty93@gmail.com>
2018-07-10 18:44:59 -0400

    Uploaded finished to-do list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,5 @@
 // check off specfic to-do by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   // if this li is already gray
   if($(this).css("color") === "rgb(128, 128, 128)") {
     // change it back to black after this click
@@ -16,3 +16,27 @@ $("li").click(function(){
 });
 // an easier way to approach this is to set up a class in the .css file
 // then use $(this).toggleClass(className); to activate or deactivate it
+
+// click on X to remove the task
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13) {
+    // get the text
+    var todoText = $(this).val();
+    //remove the tex from the input field
+    $(this).val("");
+    // create a new li
+    $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+// for the plus sign
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
